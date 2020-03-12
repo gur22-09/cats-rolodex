15ab2f86e9802559fc7e7de12e8ba0ffb46fa50a
GURPRIT <tirprug@users.noreply.github.com>
2020-03-13 05:18:47 +0530

    Updates

---
+++ precache-manifest.c459906beffa581249e9ff1546c38d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd53dc1ee3915cbe79e4614f1de514ec",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "891f6377e3e23d226996",
+    "url": "/cats-rolodex/static/css/main.e3a362c6.chunk.css"
+  },
+  {
+    "revision": "916657e0b271cb2edd85",
+    "url": "/cats-rolodex/static/js/2.7db542aa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cats-rolodex/static/js/2.7db542aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891f6377e3e23d226996",
+    "url": "/cats-rolodex/static/js/main.bd1bdb51.chunk.js"
+  },
+  {
+    "revision": "9dc09873aecf52074d08",
+    "url": "/cats-rolodex/static/js/runtime-main.e643ea7f.js"
+  }
+]);
